bff30a473e3baccb1c027aefcfb36cf88a42c2bd
kitkatyourmom <sarahlridings@gmail.com>
2023-09-17 17:35:57 -0600

    create saveBook mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -37,6 +37,18 @@ const resolvers = {
       
             return { token, user };
           },
+
+          saveBook: async (parent, { bookInfo }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: bookInfo } },
+                { new: true }
+              );
+              return updatedUser;
+            }
+            throw new AuthenticationError("You need to be logged in!");
+          },
     },
 };
 
